953646d51a2e4bae3d505ba7c2ed82ff258d1ecc
natabramov <natalie.abramov7@gmail.com>
2025-03-03 14:05:52 -0500

    fixed imports for css issue

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
-import { usePlayerStats } from "../hooks/usePlayerStats";
-import SearchBar from "../components/SearchBar";
-import styled from "styled-components";
 import Hero from "../components/LandingPage/Hero"
-import NavBar from "@components/Dashboard/Navbar";
+import NavBar from "../components/Dashboard/Navbar";
 
 export default function HomePage() {
   return (
