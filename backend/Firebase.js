688d297d371f408588be15c552b2661adc3d03be
natabramov <natalie.abramov7@gmail.com>
2025-03-03 13:59:45 -0500

    firebase commit

--- backend/Firebase.js
+++ backend/Firebase.js
@@ -1,32 +1,23 @@
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getFirestore } from "firebase/firestore";
-// import { getStorage } from "firebase/storage";
-// import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
-// // const firebaseConfig = process.env.NODE_ENV === 'production' ? {
-// //   apiKey: "",
-// //   authDomain: "",
-// //   projectId: "",
-// //   storageBucket: "",
-// //   messagingSenderId: "",
-// //   appId: "", 
-// //   measurementId: ""
-// // } : {
-// //   apiKey: "",
-// //   authDomain: "",
-// //   projectId: "",
-// //   storageBucket: "com",
-// //   messagingSenderId: "",
-// //   appId: "",
-// //   measurementId: ""
-// // }
+const firebaseConfig = {
+  apiKey: "AIzaSyAsGW-i5uI9CvIsvW7kmTvSJs3C-AGT06o",
+  authDomain: "tft-tracker-d8a59.firebaseapp.com",
+  projectId: "tft-tracker-d8a59",
+  storageBucket: "tft-tracker-d8a59.firebasestorage.app",
+  messagingSenderId: "388579002661",
+  appId: "1:388579002661:web:96952cdcb5a7b6e9f672cc",
+  measurementId: "G-TQE02QTP1Z"
+};
 
-// // // Initialize Firebase
-// // const app = initializeApp(firebaseConfig);
-// // export const auth = getAuth(app);
-// // export const storage = getStorage(app);
-// // export const database = getFirestore(app);
-// // export const analytics = () => getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
+export const analytics = () => getAnalytics(app);
 
-// // export default app
+export default app
